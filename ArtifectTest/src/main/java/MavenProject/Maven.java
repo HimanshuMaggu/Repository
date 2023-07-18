5689a64cbae9050a21d222f618b8460f2a719180
himan <himan@Himanshu>
2023-07-19 00:01:11 +0530

    Add New Project

---
+++ ArtifectTest/src/main/java/MavenProject/Maven.java
@@ -0,0 +1,12 @@
+package MavenProject;
+
+public class Maven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Maven");
+
+	}
+
+}

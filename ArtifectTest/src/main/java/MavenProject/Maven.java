c74a5ebdbd6e95da1a34f3ae611030bd67d1557b
himan <himan@Himanshu>
2023-07-21 19:00:00 +0530

    Himanshu Branch Code Updated

--- ArtifectTest/src/main/java/MavenProject/Maven.java
+++ ArtifectTest/src/main/java/MavenProject/Maven.java
@@ -6,9 +6,9 @@ public class Maven {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello Maven");
-		System.out.println("Himanshu");
+		System.out.println("Hello");
+		System.out.println("Hello Himanshu");
 		
-		System.out.println("Hello Rahul");
 	}
 
 }

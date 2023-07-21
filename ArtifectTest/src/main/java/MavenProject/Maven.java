d924a4be84fb8af07688d6d73a0d362df46bb26f
himan <himan@Himanshu>
2023-07-21 18:51:54 +0530

    Again Modified

--- ArtifectTest/src/main/java/MavenProject/Maven.java
+++ ArtifectTest/src/main/java/MavenProject/Maven.java
@@ -6,7 +6,9 @@ public class Maven {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello Maven");
+		System.out.println("Himanshu");
 		
+		System.out.println("Hello Rahul");
 	}
 
 }
